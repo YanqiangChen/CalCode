90ecbc09862cd463f68347181e1f090bd54bfcf6
yanqiang.chen <yanqiang.chen@qunar.com>
2021-01-05 16:28:27 +0800

    三数add 算法提交

--- app/src/main/java/com/example/myapplication/calculate/Tools.java
+++ app/src/main/java/com/example/myapplication/calculate/Tools.java
@@ -1,6 +1,9 @@
 package com.example.myapplication.calculate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class Tools {
     /*
@@ -88,4 +91,55 @@ public class Tools {
         }
         return min;
     }
+
+
+    /*
+    * 三数之和
+    * 最简单的方式：O(N三次方)遍历，得到N三次方个组合，去重复
+    * 比较好的方式：先进行排序，然后N的三次方进行遍历 时间复杂度O(N的三次方)
+    * 最好的方式：排序，遍历first second 然后从后往前遍历third
+    *
+    * */
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        // 枚举 a
+        for (int first = 0; first < n; ++first) {
+            // 需要和上一次枚举的数不相同
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            // c 对应的指针初始指向数组的最右端
+            int third = n - 1;
+            int target = -nums[first];
+            // 枚举 b
+            for (int second = first + 1; second < n; ++second) {
+                // 需要和上一次枚举的数不相同
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                // 需要保证 b 的指针在 c 的指针的左侧
+                while (second < third && nums[second] + nums[third] > target) {
+                    --third;
+                }
+                // 如果指针重合，随着 b 后续的增加
+                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                if (second == third) {
+                    break;
+                }
+                if (nums[second] + nums[third] == target) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
+            }
+        }
+        return ans;
+    }
+
+
+
 }
